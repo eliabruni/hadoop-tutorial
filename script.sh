5780a3ec0c95d2bda704d7791e3663f8afe19444
Elia Bruni <elia.bruni@gmail.com>
2011-11-07 10:55:19 +0100

    refact.

--- script.sh
+++ script.sh
@@ -9,4 +9,26 @@ git clone https://github.com/eliabruni/hadoop-tutorial
 cat mobydick.txt | ./mapper.py | ./reducer.py | sort -k 2 -r -n | head
 cat mobydick.txt | ./mapper.py | sort | ./reducer.py | sort -k 2 -r -n | head
 
+# put the data in hdfs
+hadoop fs -mkdir count_example
+hadoop fs -put mobydick.txt count_example
+
+# run it on hadoop
+hadoop jar $HADOOP_HOME/hadoop-streaming.jar $PARAMS -mapper mapper.py -reducer reducer.py -input count_example/mobydick.txt -output count_example/hamlet_out -file mapper.py -file reducer.py 
+
+# view the output 
+mkdir out
+hadoop fs -ls count_example/hamlet_out/
+hadoop fs -cat count_example/hamlet_out/part-* | sort -nrk 2 > out/mobydick_count.txt
+head | out/mobydick_count.txt
+
+# test that hadoop worked
+cat mobydick.txt | ./mapper.py | sort | ./reducer.py | sort -k 2 -r -n > out/mobydick_count_local.txt
+diff  out/mobydick_count.txt out/mobydick_count_local.txt
+
+# cleanup
+hadoop fs -rmr count_example
+cd ..
+rm -r count_example
+
 
